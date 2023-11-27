f201af3b31d73516a1a94e54834233558e593e3c
Pratham Madaram <90805743+BEEB0SS@users.noreply.github.com>
2023-11-27 05:18:49 -0500

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,4 +1,7 @@
 import speech_recognition as sr
+import serial
+
+ser = serial.Serial("/dev/ttyAMA0", 115200)
 
 reader = sr.Recognizer()
 mic = sr.Microphone()
@@ -10,3 +13,4 @@ while True:
     audio = reader.listen(source)
   words = reader.recognize_google(audio)
   print(words)
+  ser.write(bytes(words, 'utf-8'))
